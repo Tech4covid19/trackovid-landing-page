8e480c890eae205f16b5b603e355210c46117b15
Rui Saraiva <ruisaraiva19@gmail.com>
2020-03-19 07:45:12 +0000

    feat(footer): add variables

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import heart from "@/assets/heart.svg";
+import Typography from "@/components/typography";
 
 import styles from "./index.module.css";
-import heart from "../../assets/heart.svg";
 
-export default function Header() {
+export default function Footer() {
   return (
     <div className={styles.footer}>
-      <span>Made with</span>
-      <img className={styles.image} src={heart} alt="heart" />
-      <span>in Portugal</span>
+      <Typography variant="smallBody" weight="medium" color="darkGray">
+        Made with
+      </Typography>
+      <img className={styles.image} src={heart} alt="Heart" />
+      <Typography variant="smallBody" weight="medium" color="darkGray">
+        in Portugal
+      </Typography>
     </div>
   );
 }
