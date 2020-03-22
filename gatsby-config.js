08cf51c899e1f269b1c09617dae8d1d421c81f45
Rui Saraiva <ruisaraiva19@gmail.com>
2020-03-22 19:03:23 +0000

    feat(gtm): add gtm and update terms & conditions link

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,11 +25,16 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: "gatsby-plugin-google-tagmanager",
       options: {
-        trackingId: "trackingIDhere",
-        head: true,
-        anonymize: true,
+        id: "GTM-5567HFR",
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
       },
     },
     {
