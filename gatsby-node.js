db67a07d35acf0869c6eebc0dff7c550bd98e836
Diogo Teixeira <teixeira.diogo5@gmail.com>
2020-04-07 18:58:52 +0100

    remove comment

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,15 +1,5 @@
 const path = require(`path`);
 
-// exports.createPage({
-//   path: `/o-meu-codigo-postal/cenas`,
-//   component: path.resolve(`./src/pages/o-meu-codigo-postal.jsx`),
-//   // The context is passed as props to the component as well
-//   // as into the component's GraphQL query.
-//   context: {
-//     id: `123456`,
-//   },
-// });
-
 exports.createPages = ({ actions }) => {
   const { createPage } = actions;
 
