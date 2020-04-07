15c97b2685bcc6394222a419da49087675aa0c09
Rui Saraiva <ruisaraiva19@gmail.com>
2020-04-08 00:49:19 +0100

    fix share-link

--- src/pages/o-meu-codigo-postal.jsx
+++ src/pages/o-meu-codigo-postal.jsx
@@ -32,8 +32,7 @@ const query = graphql`
   }
 `;
 
-// eslint-disable-next-line react/prop-types
-const OMeuCodigoPostalPage = ({ location: { search } }) => {
+const OMeuCodigoPostalPage = () => {
   const data = useStaticQuery(query);
   const {
     author,
@@ -42,31 +41,34 @@ const OMeuCodigoPostalPage = ({ location: { search } }) => {
     keywords: siteKeywords,
     appImagesUrl,
   } = data.site.siteMetadata;
-  const { imageId } = queryString.parse(search);
-  const imageUrl = `https://${appImagesUrl}/${imageId}.png`;
-  checkIfImageExists(imageUrl, () => {
-    navigate("/");
-  });
-  return (
-    <Layout>
-      <Helmet>
-        <title>{siteTitle}</title>
-        <meta name="description" content={siteDescription} />
-        <meta name="keywords" content={siteKeywords} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/png" href="favicon.png" />
-        <link rel="icon" type="image/x-icon" href="favicon.ico" />
-        <meta name="language" content="pt-pt" />
-        <meta name="author" content={author} />
-        <meta property="og:title" content={siteTitle} />
-        <meta property="og:description" content={siteDescription} />
-        <meta property="og:image" content={imageUrl} />
-        <meta property="og:image:width" content="476" />
-        <meta property="og:image:height" content="714" />
-      </Helmet>
-      <CodigoPostal imageUrl={imageUrl} />
-    </Layout>
-  );
+  if (typeof window !== "undefined") {
+    const { imageId } = queryString.parse(window.location.search);
+    const imageUrl = `https://${appImagesUrl}/${imageId}.png`;
+    checkIfImageExists(imageUrl, () => {
+      navigate("/");
+    });
+    return (
+      <Layout>
+        <Helmet>
+          <title>{siteTitle}</title>
+          <meta name="description" content={siteDescription} />
+          <meta name="keywords" content={siteKeywords} />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" type="image/png" href="favicon.png" />
+          <link rel="icon" type="image/x-icon" href="favicon.ico" />
+          <meta name="language" content="pt-pt" />
+          <meta name="author" content={author} />
+          <meta property="og:title" content={siteTitle} />
+          <meta property="og:description" content={siteDescription} />
+          <meta property="og:image" content={imageUrl} />
+          <meta property="og:image:width" content="476" />
+          <meta property="og:image:height" content="714" />
+        </Helmet>
+        <CodigoPostal imageUrl={imageUrl} />
+      </Layout>
+    );
+  }
+  return null;
 };
 
 export default OMeuCodigoPostalPage;
